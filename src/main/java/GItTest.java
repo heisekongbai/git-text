e85074e45c522849c89d3433e5ba9de531b90e02
djq <djq@dsg.com>
2023-02-13 14:34:29 +0800

    git 3

--- src/main/java/GItTest.java
+++ src/main/java/GItTest.java
@@ -3,5 +3,6 @@ public class GItTest {
         System.out.println("hello Git");
         System.out.println("hello Git 2");
         System.out.println("hello Git 3");
+        System.out.println("hello Git 4");
     }
 }
